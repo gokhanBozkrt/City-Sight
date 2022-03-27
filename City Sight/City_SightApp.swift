e3e92e6a7a565adc64eff48e6e3b0b7b9fd0fab3
gokhanBozkrt <99559446+gokhanBozkrt@users.noreply.github.com>
2022-03-27 11:26:29 +0300

    Initial Commit

---
+++ City Sight/City_SightApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightApp.swift
+//  City Sight
+//
+//  Created by Gokhan Bozkurt on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct City_SightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchView()
+        }
+    }
+}
---
+++ City Sight/LaunchView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City Sight
+//
+//  Created by Gokhan Bozkurt on 27.03.2022.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
